b08a5a516edf4637485d469bae35d241cd1e7f26
mp3-tech <61710mp3@gmail.com>
2026-04-20 15:08:38 +0800

    Fix path to current directory

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
 import React from "react";
-// 如果報錯，請把下面的 components 改成 Components (如果你的資料夾開頭是大寫)
-import ContainerBlock from "../components/ContainerBlock";
-import Hero from "../components/Hero";
-import AboutMe from "../components/AboutMe";
-import Experience from "../components/Experience";
+
+import ContainerBlock from "./components/ContainerBlock";
+import Hero from "./components/Hero";
+import AboutMe from "./components/AboutMe";
+import Experience from "./components/Experience";
 
 export default function Home() {
   return (
